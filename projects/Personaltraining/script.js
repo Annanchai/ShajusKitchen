2821415ca5f5ce637036216d7775f34ee91753c0
unknown <Administrator@majaneyecentre.com>
2022-07-07 21:34:12 +0400

    Projects added responsiveness pending

---
+++ projects/Personaltraining/script.js
@@ -0,0 +1,7 @@
+function opennav() {
+    document.getElementById("mobileMenu").style.display = "inline-block";
+}
+
+function closenav() {
+    document.getElementById("mobileMenu").style.display = "none";
+}
